ebf3c40956044ce38e55b1fb37dbe04921938fa1
ALIJON0999 <ubaydullayevalijon4@gmail.com>
2023-12-02 15:58:50 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+  # 1- vazifa
+son = int(input("Son: "))
+if son % 2 == 0 :
+    print(f"{ son } soni 2 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 3 == 0:
+    print(f"{ son } soni 3 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 4 == 0:
+    print(f"{ son } soni 4 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 5 == 0:
+    print(f"{ son } soni 5 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 6 == 0:
+    print(f"{ son } soni 6 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 7 == 0:
+    print(f"{ son } soni 7 soniga qildiqsiz bo'linadi. ")
+
+elif  son % 8 == 0:
+    print(f"{ son } soni 8 soniga qildiqsiz bo'linadi. ")
+
+
+elif  son % 9 == 0:
+    print(f"{ son } soni 9 soniga qildiqsiz bo'linadi. ")
+    # 1-vazifa tugadi
+
+else:
+    print( "Bu son yoki o'ziga yoki 1 ga bulinadi unisini uzing top.")
+# 2-vazifa
+
+people = int(input("O'quvchilar sonini kiriting: "))
+
+m = int(input("Savatdagi olmalar sonini kiriting: "))
+
+print(   (people % m)  )
+
